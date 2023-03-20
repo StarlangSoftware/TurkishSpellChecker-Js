fd55f647746657bf1336a0b1986a72d4d98b86a9
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-03-20 15:29:44 +0300

    Updated finite state machine.

--- tests/TrieBasedSpellCheckerTest.ts
+++ tests/TrieBasedSpellCheckerTest.ts
@@ -14,7 +14,7 @@ describe('TrieBasedSpellCheckerTest', function() {
         let nGram = new NGram<string>("ngram.txt");
         nGram.calculateNGramProbabilitiesSimple(new NoSmoothing<string>());
         it('testSpellCheck', function() {
-            this.timeout(10000);
+            this.timeout(12000);
             let original = [new Sentence("antibiyotik direnci bir mikroorganizmanın antibiyotiklerin etkilerine karşı durabilme yeteneğidir"),
                 new Sentence("bugünkü ortaöğretim sisteminin oluşumu Cumhuriyet döneminde gerçekleşmiştir"),
                 new Sentence("bilinçsizce dağıtılan kredilerin sorun yaratmayacağını düşünmelerinin nedeni bankaların büyüklüğünden kaynaklanıyordu"),
